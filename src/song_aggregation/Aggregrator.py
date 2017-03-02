a8570b821a1b659dfafad60c608052a18854774f
marlonfl <marlonfl@informatik.uni-bremen.de>
2017-03-02 22:36:23 +0100

    add db

--- src/song_aggregation/Aggregrator.py
+++ src/song_aggregation/Aggregrator.py
@@ -1,13 +1,20 @@
-import sqlite3
+import sqlite3 as sql
 import os
 
 
 class Aggregrator(object):
     db_path = "../../data/songs.db"
+    cur = None
 
     def __init__(self):
+
         # creates empty db if it doesn't exist
         open(self.db_path, 'a').close()
+        conn = sql.connect(self.db_path)
+        self.cur = conn.cursor()
+        self.cur.execute('''CREATE TABLE titles (artist, title)''')
+        conn.commit()
+        conn.close()
 
 if __name__ == "__main__":
     a = Aggregrator()
